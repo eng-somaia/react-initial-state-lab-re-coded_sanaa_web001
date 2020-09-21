fa2fce2420d4e0521d3ef037cf6b8e0e06371389
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-21 10:21:58 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -5,7 +5,7 @@ export default class Bomb extends Component {
   constructor(props){
     super()
     this.state = {
-      secondsLeft: this.props.initialCount
+      secondsLeft: props.initialCount
     }
   }
 
