6c519b5f067bb12e0b1eff0052bb3be6979f1cd3
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-21 10:18:23 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -3,25 +3,17 @@ import React, { Component } from 'react';
 
 export default class Bomb extends Component {
   constructor(props){
-    super(props)
+    super()
     this.state = {
-      secondsLeft: props.initialCount
+      secondsLeft: this.props.initialCount
     }
   }
-  countDown = (this.state.secondsLeft) =>{
-    let x = setInterval(()=>{
-      newSecondsLeft = this.state.secondsLeft - 1
-      this.setState({
-        secondsLeft: newSecondsLeft
-      })
-
-    },1000)
-  }
+  
   render() {
     const secondsLeft = initialCount;
     return (
       <div>
-      second'{this.state.secondsLeft} seconds left before I go boom!'
+      this.state.secondsLeft === 0 ? 'Boom!' : '{this.state.secondsLeft} seconds left before I go boom!'
       this.countDown();
       </div>
     )
