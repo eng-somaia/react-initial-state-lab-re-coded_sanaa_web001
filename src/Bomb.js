2820669483244dd5e139eefe296cb3f000af33a3
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-21 09:48:06 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -2,18 +2,9 @@ import React, { Component } from 'react';
 
 
 export default class Bomb extends Component {
-constructor(){
-  super()
-  this.state = {
-  currentSlideIndex: 0
-}
-}
-
   render() {
     return (
-      <div>
-      'I am on slide this.state.currentSlideIndex'
-      </div>
+      <div></div>
     )
   }
 }
