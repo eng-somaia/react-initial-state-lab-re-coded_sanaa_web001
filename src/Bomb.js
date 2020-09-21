76fe3e3208dbfb7cef36846e7bed6a5f42844dca
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-21 10:11:49 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -2,9 +2,29 @@ import React, { Component } from 'react';
 
 
 export default class Bomb extends Component {
+  constructor(props){
+    super()
+    this.state = {
+      secondsLeft: this.props.initialCount
+    }
+  }
+  countDown = () =>{
+    let x = setInterval(()=>{
+      newSecondsLeft = this.state.secondsLeft - 1
+      this.setState({
+        secondsLeft: newSecondsLeft
+      })
+      if (newSecondsLeft === 0) {
+         clearInterval(x);
+         'Boom!'}
+    },1000)
+  } 
   render() {
+    const secondsLeft = initialCount
     return (
-      <div></div>
+      <div>
+      second'{this.state.secondsLeft} seconds left before I go boom!'
+      </div>
     )
   }
 }
