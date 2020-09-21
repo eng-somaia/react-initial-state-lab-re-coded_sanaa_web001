2923beb97e52370741b28a276ec8d9394b4df24e
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-21 10:15:12 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 
 export default class Bomb extends Component {
   constructor(props){
-    super()
+    super(props)
     this.state = {
       secondsLeft: this.props.initialCount
     }
