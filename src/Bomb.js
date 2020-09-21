eb867dc0a731b5b735a9c8a2327461e4a59ad114
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-21 10:24:18 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -13,7 +13,7 @@ export default class Bomb extends Component {
 
     return (
       <div>
-      this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`
+    {  this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`}
       </div>
     )
   }
